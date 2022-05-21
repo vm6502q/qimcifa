e7b741f707fc2a78daccdf884ed8c15142e8f832
Daniel Strano <stranoj@gmail.com>
2022-05-21 14:06:49 -0400

    Remove extraneous ternary

--- qimcifa.cpp
+++ qimcifa.cpp
@@ -166,7 +166,7 @@ int main()
     const bitCapInt toFactorMin2 = toFactor - 2U;
     const uint64_t randRemainder = (uint64_t)(toFactorMin2 % maxPow);
     const uint64_t maxLongLongs = (log2(toFactorMin2) + (!isPowerOfTwo(toFactorMin2) ? 1U : 0U) + (wordSize - 1U)) / wordSize;
-    const uint64_t maxLongLongsMin1 = maxLongLongs ? (maxLongLongs - 1U) : 0U;
+    const uint64_t maxLongLongsMin1 = maxLongLongs - 1U;
 
     std::random_device rand_dev;
     std::mt19937 rand_gen(rand_dev());
