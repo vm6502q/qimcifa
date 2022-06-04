7122bb3675accce077713ce80ceb2480ee39d7cc
Daniel Strano <stranoj@gmail.com>
2022-06-04 13:34:05 -0400

    Remove redundant code block

--- src/qimcifa.cpp
+++ src/qimcifa.cpp
@@ -324,13 +324,11 @@ int main()
             distPart >>= WORD_SIZE;
         }
         std::reverse(rDist.begin(), rDist.end());
-#else
-#if IS_RSA_SEMIPRIME
+#elif IS_RSA_SEMIPRIME
         // Euler's totient is the product of 2 even numbers.
         rDist.push_back(rand_dist(rMin >> 1U, rMax >> 1U));
 #else
         rDist.push_back(rand_dist(rMin, rMax));
-#endif
 #endif
 
         for (;;) {
@@ -343,7 +341,7 @@ int main()
                     base |= baseDist[i](rand_gen);
                 }
 #if IS_RSA_SEMIPRIME
-                base = (base + (fullMinR >> 1U)) << 1U + 1U;
+                base = ((base + (fullMinR >> 1U)) << 1U) | 1U;
 #else
                 base += 2U;
 #endif
@@ -413,11 +411,11 @@ int main()
                     // Since our output is r rather than y, we can skip the continued fractions step.
                     // As a "classical" optimization, since \phi(toFactor) and factor bounds overlap,
                     // we first check if our guess for r is already a factor.
-                    if ((toFactor % (r + 1)) == 0) {
+                    if ((toFactor % (r | 1)) == 0) {
                         // Inform the other threads on this node that we've succeeded and are done:
                         isFinished = true;
 
-                        PRINT_SUCCESS((r + 1), toFactor / (r + 1), toFactor, "Success (on r trial division): Found ");
+                        PRINT_SUCCESS((r | 1), toFactor / (r | 1), toFactor, "Success (on r trial division): Found ");
                         return;
                     }
 #else
@@ -434,17 +432,6 @@ int main()
                         return;
                     }
 #endif
-
-                    // Since our output is r rather than y, we can skip the continued fractions step.
-                    // As a "classical" optimization, since \phi(toFactor) and factor bounds overlap,
-                    // we first check if our guess for r is already a factor.
-                    if ((toFactor % (r + 1)) == 0) {
-                        // Inform the other threads on this node that we've succeeded and are done:
-                        isFinished = true;
-
-                        PRINT_SUCCESS((r + 1), toFactor / (r + 1), toFactor, "Success (on r trial division): Found ");
-                        return;
-                    }
 
                     const bitCapInt apowrhalf = uipow(base, r) % toFactor;
                     bitCapInt f1 = gcd(apowrhalf + 1U, toFactor);
