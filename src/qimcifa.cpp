e957f5b1aa8e2528948344320c3d188c8c45604e
Daniel Strano <stranoj@gmail.com>
2022-06-06 07:09:17 -0400

    Fix range bounds

--- src/qimcifa.cpp
+++ src/qimcifa.cpp
@@ -223,14 +223,14 @@ int main()
     const bitCapInt minPrime = primeDict[primeBits].size() ? primeDict[primeBits][0] : ((ONE_BCI << (primeBits - 1U)) + 1U);
     const bitCapInt maxPrime = primeDict[primeBits].size() ? primeDict[primeBits][1] : ((ONE_BCI << primeBits) - 1U);
     const bitCapInt fullMinBase = (((toFactor / maxPrime) < minPrime) ? minPrime : ((toFactor / maxPrime) | 1U)) >> 1U;
-    const bitCapInt fullMaxBase = (toFactor / ((fullMinBase << 1U) | 1U)) >> 1U;
+    const bitCapInt fullMaxBase = (((toFactor / minPrime) > maxPrime) ? maxPrime : ((toFactor / minPrime) | 1U)) >> 1U;
 #else
     const bitCapInt fullMinBase = 2U;
     const bitCapInt fullMaxBase = toFactor / 2U;
 #endif
     const bitCapInt nodeRange = (fullMaxBase + 1U - fullMinBase) / nodeCount;
     const bitCapInt nodeMin = fullMinBase + nodeRange * nodeId;
-    const bitCapInt nodeMax = ((nodeId + 1U) == nodeCount) ? fullMaxBase : (fullMinBase + nodeRange * (nodeId + 1U) - 1U);
+    const bitCapInt nodeMax = ((nodeId + 1U) == nodeCount) ? fullMaxBase : (fullMinBase + nodeRange * (nodeId + 1U));
 
     auto iterClock = std::chrono::high_resolution_clock::now();
 
@@ -255,7 +255,7 @@ int main()
 
         const bitCapInt threadRange = (nodeMax + 1U - nodeMin) / cpuCount;
         const bitCapInt rMin = nodeMin + threadRange * cpu;
-        const bitCapInt rMax = ((cpu + 1U) == cpuCount) ? nodeMax : (nodeMin + threadRange * (cpu + 1U) - 1U);
+        const bitCapInt rMax = ((cpu + 1U) == cpuCount) ? nodeMax : (nodeMin + threadRange * (cpu + 1U));
         std::vector<rand_dist> rDist(rangeRange(rMax - rMin));
 
         for (;;) {
