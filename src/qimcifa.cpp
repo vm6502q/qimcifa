45248ae89294d872fd839a64857c2a311a0ab83f
Daniel Strano <stranoj@gmail.com>
2022-06-12 08:20:45 -0400

    Adjust t.d. floor

--- src/qimcifa.cpp
+++ src/qimcifa.cpp
@@ -54,7 +54,7 @@ inline size_t pickTrialDivisionLevel(size_t qubitCount)
         return TRIAL_DIVISION_LEVEL_OVERRIDE;
     }
 #endif
-    if (qubitCount <= 56) {
+    if (qubitCount <= 58) {
         return 43;
     }
     if (qubitCount <= 66) {
