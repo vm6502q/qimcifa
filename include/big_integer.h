362c478c721126c86ed2b97f7d834bb320cd8285
Daniel Strano <stranoj@gmail.com>
2023-12-13 18:24:36 -0500

    Fix bounds-check issue in multiplication

--- include/big_integer.h
+++ include/big_integer.h
@@ -493,7 +493,7 @@ BigInteger bi_mul(const BigInteger* left, const BigInteger* right)
                 BIG_INTEGER_WORD temp = result.bits[j];
                 result.bits[j] += partMul.bits[j];
                 int k = j;
-                while ((k < BIG_INTEGER_WORD_SIZE) && (temp > result.bits[k])) {
+                while ((k < BIG_INTEGER_MAX_WORD_INDEX) && (temp > result.bits[k])) {
                     temp = result.bits[++k]++;
                 }
             }
