a60deb0f1cf2217fe77eb3dcac554dde2bbf66a4
Daniel Strano <stranoj@gmail.com>
2024-03-03 16:34:56 -0500

    Only 50 primes

--- src/qimcifa.cpp
+++ src/qimcifa.cpp
@@ -313,14 +313,14 @@ int main()
 #endif
 
     // First 1000 primes
-    // (Only including first 100 in program)
+    // (Only including first 50 in program)
     // Source: https://gist.github.com/cblanc/46ebbba6f42f61e60666#file-gistfile1-txt
     const std::vector<unsigned> trialDivisionPrimes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
         61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
-        181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
-        311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439,
-        443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541 //, 547, 557, 563, 569, 571, 577, 587,
+        181, 191, 193, 197, 199, 211, 223, 227, 229 //, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307,
 #if 0
+        311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439,
+        443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587,
         593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727,
         733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
         881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021,
@@ -379,10 +379,7 @@ int main()
     // }
 
     bitCapIntInput toFactor;
-    size_t nodeCount = 1U;
-    size_t nodeId = 0U;
     size_t primeBitsOffset = 1U;
-    int64_t tdLevel = -1;
 
     std::cout << "Number to factor: ";
     std::cin >> toFactor;
@@ -405,6 +402,8 @@ int main()
     std::cin >> primeBitsOffset;
 #endif
 
+    size_t nodeCount = 1U;
+    size_t nodeId = 0U;
 #if IS_DISTRIBUTED
     std::cout << "You can split this work across nodes, without networking!" << std::endl;
     do {
@@ -425,8 +424,12 @@ int main()
     }
 #endif
 
+    int64_t tdLevel = -1;
     std::cout << "Reverse trial division level (-1 for calibration file): ";
     std::cin >> tdLevel;
+    if (tdLevel >= (int64_t)trialDivisionPrimes.size()) {
+        tdLevel = trialDivisionPrimes.size() - 1U;
+    }
     tdLevel = pickTrialDivisionLevel<bitCapIntInput>(tdLevel, nodeCount);
 
     const unsigned highestPrime = trialDivisionPrimes[tdLevel];
