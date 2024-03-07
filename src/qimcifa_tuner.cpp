a10c9f3e68b80c4f9cb62ddd30546dde98a4fdd2
Daniel Strano <stranoj@gmail.com>
2024-03-07 14:50:03 -0500

    Fix lower bound

--- src/qimcifa_tuner.cpp
+++ src/qimcifa_tuner.cpp
@@ -418,7 +418,7 @@ CsvRow mainBody(const bitCapInt& toFactor, const int64_t& tdLevel, const size_t&
         fullMinBase *= trialDivisionPrimes[primeIndex];
     }
     // The exact lower bound CANNOT be a multiple of any reverse trial division prime.
-    fullMinBase += trialDivisionPrimes[tdLevel - 1] - 1U;
+    ++fullMinBase;
     bitCapInt fullRange = fullMaxBase + 1U - fullMinBase;
     for (int64_t primeIndex = 0; primeIndex < tdLevel; ++primeIndex) {
         // The truncation here is a conservative bound, but it's exact if we
