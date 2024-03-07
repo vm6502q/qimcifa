8c193cd50be34a8a9776d505bed93dbbfd5da4f9
Daniel Strano <stranoj@gmail.com>
2024-03-07 16:44:36 -0500

    Map base case (0-level) to base case (0 numeral)

--- src/qimcifa_tuner.cpp
+++ src/qimcifa_tuner.cpp
@@ -417,7 +417,7 @@ CsvRow mainBody(const bitCapInt& toFactor, const int64_t& tdLevel, const size_t&
     for (int64_t primeIndex = tdLevel - 1; primeIndex >= 0; --primeIndex) {
         fullMinBase += fullMinBase / (trialDivisionPrimes[primeIndex] - 1U) + 1U;
     }
-    ++fullMinBase;
+    --fullMinBase;
     bitCapInt fullRange = fullMaxBase + 1U - fullMinBase;
     for (int64_t primeIndex = 0; primeIndex < tdLevel; ++primeIndex) {
         // The truncation here is a conservative bound, but it's exact if we
