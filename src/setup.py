e388371388dcccce1917112d951c589a62799546
Daniel Strano <stranoj@gmail.com>
2024-03-14 14:40:29 -0400

    pybind11 module

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+cpp_args = ['-std=c++11']
+
+ext_modules = [
+    Extension(
+        'prime_gen',
+        ['prime_gen.cpp'],
+        include_dirs=['pybind11/include'],
+        language='c++',
+        extra_compile_args = cpp_args,
+    ),
+]
+
+setup(
+    name='prime_gen',
+    version='1.0',
+    author='Dan Strano',
+    author_email='dan@unitary.fund',
+    description='pybind11 plugin to generate prime numbers',
+    ext_modules=ext_modules,
+)
