ed86aff6c1d324607509f3981822406324c4f6a2
Daniel Strano <stranoj@gmail.com>
2024-03-13 20:12:00 -0400

    Trying to remove 2/3 multiples

--- src/prime_generator.cpp
+++ src/prime_generator.cpp
@@ -5,7 +5,7 @@
  
 void SieveOfEratosthenes(const int& n)
 {
-    std::vector<int> knownPrimes = { 2, 3, 5 };
+    std::vector<int> knownPrimes = { 2, 3 };
 
     int cardinality = n;
     for (int p : knownPrimes) {
@@ -15,17 +15,21 @@ void SieveOfEratosthenes(const int& n)
     // Create a boolean array "prime[0..n]" and initialize
     // all entries it as true. A value in prime[i] will
     // finally be false if i is Not a prime, else true.
-    bool prime[cardinality + 1];
-    memset(prime, true, sizeof(prime));
+    std::vector<bool> notPrime(cardinality + 1);
  
-    for (int p = 7; p * p <= n; p++) {
-        int o = p;
-        for (int j = knownPrimes.size() - 1; j >= 0; --j) {
-            const int q = knownPrimes[j];
-            o = ((q - 1) * (o + 1)) / q;
+    int o = 2;
+    while (true) {
+        int p = o;
+
+        // Make this NOT a multiple of 2 or 3.
+        p = p + (p >> 1U);
+        p = (p << 1U) - 1U;
+
+        if ((p * p) > n) {
+            break;
         }
         // If prime[o] is not changed, then it is a prime
-        if (prime[o] == true) {
+        if (!notPrime[o]) {
             // Update all multiples of p greater than or
             // equal to the square of it numbers which are
             // multiple of p and are less than p^2 are
@@ -36,9 +40,10 @@ void SieveOfEratosthenes(const int& n)
                     const int q = knownPrimes[j];
                     o = ((q - 1) * (o + 1)) / q;
                 }
-                prime[o] = false;
+                notPrime[o] = true;
             }
         }
+        ++o;
     }
 
     for (int p : knownPrimes) {
@@ -46,25 +51,12 @@ void SieveOfEratosthenes(const int& n)
     }
  
     // Print all prime numbers
-    for (int o = 2; o <= cardinality; o+=10) {
-        for (int batchItem = 1; batchItem < 7; ++batchItem) {
-            int p = o + batchItem;
-            if (prime[p]) {
-                // Make this NOT a multiple of 2 or 3.
-                p = p + (p >> 1U);
-                p = (p << 1U) - 1U;
-                std::cout << p << " ";
-            }
-        }
-
-        for (int batchItem = 8; batchItem < 10; ++batchItem) {
-            int p = o + batchItem;
-            if (prime[p]) {
-                // Make this NOT a multiple of 2 or 3.
-                p = p + (p >> 1U);
-                p = (p << 1U) - 1U;
-                std::cout << p << " ";
-            }
+    for (int o = 2; o <= cardinality; ++o) {
+        if (!notPrime[o]) {
+            // Make this NOT a multiple of 2 or 3.
+            int p = o + (o >> 1U);
+            p = (p << 1U) - 1U;
+            std::cout << p << " ";
         }
     }
 }
@@ -72,7 +64,7 @@ void SieveOfEratosthenes(const int& n)
 // Driver Code
 int main()
 {
-    int n = 30;
+    int n = 60;
     std:: cout << "Following are the prime numbers smaller than or equal to " << n << ":" << std::endl;
     SieveOfEratosthenes(n);
     return 0;
