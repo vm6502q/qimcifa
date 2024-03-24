92d62c7e9cb0867cf3f8e5f383b53af958d81f6f
Daniel Strano <stranoj@gmail.com>
2024-03-24 09:12:22 -0400

    Code reuse

--- src/prime_gen.cpp
+++ src/prime_gen.cpp
@@ -10,192 +10,11 @@
 // number under trial. Multiples of 2, 3, 5, 7, and 11 can
 // be entirely skipped in loop enumeration.
 
-#include "config.h"
-
-#include <iostream>
-#include <vector>
-
-#include <boost/dynamic_bitset.hpp>
-#if USE_GMP
-#include <boost/multiprecision/gmp.hpp>
-#elif USE_BOOST
-#include <boost/multiprecision/cpp_int.hpp>
-#else
-#include "big_integer.hpp"
-#endif
-
-#include "dispatchqueue.hpp"
+#include "prime_generator.hpp"
 
 #include <pybind11/pybind11.h>
 
-const size_t BATCH_SIZE = 1 << 10;
-const size_t cpuCount = std::thread::hardware_concurrency();
-DispatchQueue dispatch(cpuCount);
-
-#if BIG_INT_BITS < 33
-typedef uint32_t BigInteger;
-#elif BIG_INT_BITS < 65
-typedef uint64_t BigInteger;
-#else
-#if USE_GMP
-typedef boost::multiprecision::mpz_int BigInteger;
-#elif USE_BOOST
-typedef boost::multiprecision::number<boost::multiprecision::cpp_int_backend<BIG_INT_BITS, BIG_INT_BITS,
-    boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>
-    BigInteger;
-#else
-typedef BigInteger BigInteger;
-#endif
-#endif
-
-inline BigInteger sqrt(const BigInteger& toTest)
-{
-    // Otherwise, find b = sqrt(b^2).
-    BigInteger start = 1U, end = toTest >> 1U, ans = 0U;
-    do {
-        const BigInteger mid = (start + end) >> 1U;
-
-        // If toTest is a perfect square
-        const BigInteger sqr = mid * mid;
-        if (sqr == toTest) {
-            return mid;
-        }
-
-        if (sqr < toTest) {
-            // Since we need floor, we update answer when mid*mid is smaller than p, and move closer to sqrt(p).
-            start = mid + 1U;
-            ans = mid;
-        } else {
-            // If mid*mid is greater than p
-            end = mid - 1U;
-        }
-    } while (start <= end);
-
-    return ans;
-}
-
-inline BigInteger backward(const BigInteger& n) {
-    return ((~((~n) | 1U)) / 3U) + 1U;
-}
-
-inline BigInteger forward(const BigInteger& p) {
-    // Make this NOT a multiple of 2 or 3.
-    return (p << 1U) + (~(~p | 1U)) - 1U;
-}
-
-inline BigInteger backward5(BigInteger n) {
-    n = ((n + 1U) << 2U) / 5;
-    n = ((n + 1U) << 1U) / 3;
-    return (n + 1U) >> 1U;
-}
-
-bool isMultipleParallel(const BigInteger& p, const size_t& nextPrimeIndex, const size_t& highestIndex,
-    const std::vector<BigInteger>& knownPrimes) {
-    const size_t _BATCH_SIZE = BATCH_SIZE;
-    const size_t maxLcv = (highestIndex - nextPrimeIndex) / BATCH_SIZE;
-    dispatch.resetResult();
-    for (size_t i = 0; i < maxLcv; ++i) {
-        size_t j = i * BATCH_SIZE + nextPrimeIndex;
-        dispatch.dispatch([&knownPrimes, &p, _BATCH_SIZE, j]() {
-            for (size_t k = 0; k < _BATCH_SIZE; ++k) {
-                if ((p % knownPrimes[j + k]) == 0) {
-                    return true;
-                }
-            }
-            return false;
-        });
-    }
-
-    return dispatch.finish();
-}
-
-bool isMultiple(const BigInteger& p, size_t nextIndex, const std::vector<BigInteger>& knownPrimes) {
-    const BigInteger sqrtP = sqrt(p);
-    const size_t highestIndex = std::distance(knownPrimes.begin(), std::upper_bound(knownPrimes.begin(), knownPrimes.end(), sqrtP));
-
-    const size_t diff = highestIndex - nextIndex;
-    if ((highestIndex > nextIndex) && ((diff >> 1U) > BATCH_SIZE)) {
-        if (isMultipleParallel(p, nextIndex, highestIndex, knownPrimes)) {
-            return true;
-        }
-    }
-    nextIndex = diff % BATCH_SIZE;
-
-    for (size_t i = nextIndex; i < highestIndex; ++i) {
-        if ((p % knownPrimes[i]) == 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-template <typename BigInteger>
-bool isMultiple(const BigInteger& p, const std::vector<BigInteger>& knownPrimes) {
-    for (const BigInteger& prime : knownPrimes) {
-        if ((p % prime) == 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-template <typename BigInteger>
-boost::dynamic_bitset<size_t> wheel_inc(std::vector<BigInteger> primes, BigInteger limit) {
-    BigInteger radius = 1U;
-    for (const BigInteger& i : primes) {
-        radius *= i;
-    }
-    if (limit < radius) {
-        radius = limit;
-    }
-    const BigInteger prime = primes.back();
-    primes.pop_back();
-    std::vector<bool> o;
-    for (BigInteger i = 1U; i <= radius; ++i) {
-        if (!isMultiple(i, primes)) {
-            o.push_back((i % prime) == 0);
-        }
-    }
-
-    boost::dynamic_bitset<size_t> output(o.size());
-    for (size_t i = 0U; i < o.size(); ++i) {
-        output[i] = o[i];
-    }
-    output >>= 1U;
-
-    return output;
-}
-
-template <typename BigInteger>
-std::vector<boost::dynamic_bitset<size_t>> wheel_gen(const std::vector<BigInteger>& primes, BigInteger limit) {
-    std::vector<boost::dynamic_bitset<size_t>> output;
-    std::vector<BigInteger> wheelPrimes;
-    for (const BigInteger& p : primes) {
-        wheelPrimes.push_back(p);
-        output.push_back(wheel_inc(wheelPrimes, limit));
-    }
-    return output;
-}
-
-inline size_t GetWheelIncrement(std::vector<boost::dynamic_bitset<size_t>>& inc_seqs) {
-    size_t wheelIncrement = 0U;
-    bool is_wheel_multiple = false;
-    do {
-        for (size_t i = 0; i < inc_seqs.size(); ++i) {
-            boost::dynamic_bitset<size_t>& wheel = inc_seqs[i];
-            is_wheel_multiple = wheel.test(0U);
-            wheel >>= 1U;
-            if (is_wheel_multiple) {
-                wheel[wheel.size() - 1U] = true;
-                break;
-            }
-        }
-        wheelIncrement++;
-    } while (is_wheel_multiple);
-
-    return wheelIncrement;
-}
-
+namespace qimcifa {
 std::vector<BigInteger> TrialDivision(const BigInteger& n)
 {
     // First 3 primes
@@ -329,6 +148,7 @@ std::vector<BigInteger> SieveOfEratosthenes(const BigInteger& n)
             if (lcv5 > 4) {
                 lcv5 -= 5;
             }
+
             if (lcv5) {
                 notPrime[(size_t)backward5(i)] = true;
             }
@@ -362,6 +182,9 @@ std::vector<BigInteger> SieveOfEratosthenes(const BigInteger& n)
 
     return knownPrimes;
 }
+} // namespace qimcifa
+
+using namespace qimcifa;
 
 PYBIND11_MODULE(prime_gen, m) {
     m.doc() = "pybind11 plugin to generate prime numbers";
