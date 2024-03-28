f28fa3f7c0997aba891c921dda9e37902f8082e9
Daniel Strano <stranoj@gmail.com>
2024-03-28 09:09:42 -0400

    Fix off-by-1

--- include/prime_generator.hpp
+++ include/prime_generator.hpp
@@ -93,7 +93,7 @@ inline BigInteger backward7(BigInteger n) {
         1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 121,
         127, 131, 137, 139, 143, 149, 151, 157, 163, 167, 169, 173, 179, 181, 187, 191, 193, 197, 199, 209
     };
-    const auto p = std::upper_bound(m, m + 48U, n % 210U);
+    const auto p = std::lower_bound(m, m + 48U, n % 210U);
     return std::distance(m, p) + 48U * (n / 210U) + 1U;
 }
 
