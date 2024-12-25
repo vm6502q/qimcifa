42b0dc0bf729b64e359d5ed7714d7307574c6235
WrathfulSpatula <stranoj@gmail.com>
2024-12-25 09:54:48 -0500

    Restore time estimate (still not right)

--- src/qimcifa_tuner.cpp
+++ src/qimcifa_tuner.cpp
@@ -72,8 +72,6 @@ double mainBody(const BigInteger& toFactor, const uint64_t& tdLevel, const std::
 
     std::vector<BigInteger> smoothNumbers;
     auto iterClock = std::chrono::high_resolution_clock::now();
-    batchNumber = 0U;
-    batchBound = 1U;
     getSmoothNumbers(toFactor, inc_seqs, offset, iterClock);
 
     return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - iterClock).count() * 1e-10;
@@ -182,9 +180,9 @@ int main() {
         // Test
         const double time = mainCase(toFactor, i);
 #if BIG_INTEGER_BITS > 64 && !USE_BOOST && !USE_GMP
-        oSettingsFile << i << " " << range << " " << time << " " << (bi_to_double(range) * time / SMALLEST_WHEEL) << std::endl;
+        oSettingsFile << i << " " << range << " " << time << " " << (time * bi_to_double(range) / BIGGEST_WHEEL) << std::endl;
 #else
-        oSettingsFile << i << " " << range << " " << time << " " << (range.convert_to<double>() * time / SMALLEST_WHEEL) << std::endl;
+        oSettingsFile << i << " " << range << " " << time << " " << (time * range.convert_to<double>() / BIGGEST_WHEEL) << std::endl;
 #endif
     }
     oSettingsFile.close();
